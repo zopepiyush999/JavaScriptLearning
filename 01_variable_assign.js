44f095c1dbb33078292946012037f8b3f746e7a0
Piyush Zope <zopepiyush999@gmail.com>
2022-12-08 19:02:20 +0530

    Variable Initialize, Declaration and Update
    Variable Initialize, Declaration and Update

---
+++ 01_variable_assign.js
@@ -0,0 +1,23 @@
+var myFullName;                 //      Before Initialization
+console.log(myFullName);
+
+var myFullName;
+myFullName = "Piyush Zope";      //     Initialize value
+console.log(myFullName);
+
+var myFullName;
+myFullName = "Piyush Atmaram Zope";     //      Update Value
+console.log(myFullName);
+
+var myLove;
+myLove = "I love only JS";             //       Declare and Initialize the Variable Value
+console.log(myLove);
+
+var firstName = "Piyush";
+var lastName = "Zope";
+var age = "26";
+var address = "Pune";
+var pincode = "412105";
+var gender = "Male";
+var is_married ="No";
+console.log(firstName, lastName, age, address, pincode, gender, is_married);
---
+++ variable.js
@@ -0,0 +1,10 @@
+var myName;             //  Variable Declaration
+myName = "Piyush Zope";  //  Variable Initialization 
+console.log(myName);
+
+
+var myFrdName = "Sachin Tendulkar";  // Variable Declaration Initialization at the same time
+console.log(myFrdName);
+
+myFrdName = "MS Dhoni";     //  Variable Update
+console.log(myFrdName);
