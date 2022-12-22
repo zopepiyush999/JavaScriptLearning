8810ea17981b0a01cd2f97db891e7c7dfea5537f
Piyush Zope <zopepiyush999@gmail.com>
2022-12-22 15:26:44 +0530

    Vowels count
    Vowels count

---
+++ 05_vowel_count_assign.js
@@ -0,0 +1,16 @@
+function  counterCharA(string){
+var counter = 0;
+for (let index = 0; index < string.length; index++) {
+    var char = string.charAt(index);
+    if (char == 'a' || char == 'A'){
+        console.log( char);
+        counter = counter + 1;
+    }
+    }
+    console.log(`${string}${counter}`);
+}
+counterCharA(`I AM Learning JavaScript, The Language of internet :- `);
+
+console.log("---------------------------------------------------------------");
+
+counterCharA(`My favourite movie is LAggAn :- `);
